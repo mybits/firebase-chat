7bb777b1023ba93bd60fb8c3b714dd83f966a372
mybits <paula.stepinska@gmail.com>
2016-07-25 17:23:34 +0100

    Display user name and profile photo in the top bar

--- web-start/scripts/main.js
+++ web-start/scripts/main.js
@@ -126,8 +126,8 @@ FriendlyChat.prototype.signOut = function() {
 FriendlyChat.prototype.onAuthStateChanged = function(user) {
   if (user) { // User is signed in!
     // Get profile pic and user's name from the Firebase user object.
-    var profilePicUrl = null;   // TODO(DEVELOPER): Get profile pic.
-    var userName = null;        // TODO(DEVELOPER): Get user's name.
+    var profilePicUrl = user.photoUrl;
+    var userName = user.displayName;
 
     // Set the user's profile pic and name.
     this.userPic.style.backgroundImage = 'url(' + profilePicUrl + ')';
